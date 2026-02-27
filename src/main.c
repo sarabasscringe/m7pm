c4af23cca2c3573d9e86f9b9d7c3ffa58ed96add
sarabasscringe <saraespedal8@gmail.com>
2026-02-27 18:10:00 +0100

    started rough ls and config

--- src/main.c
+++ src/main.c
@@ -3,8 +3,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MAX_ARGS 8
+#define MAX_ARGS 16
 #define MAX_CHARS 64
+// thanks to whoever posted these on stack overflow youre cool
+#define col_red     "\x1b[31m"
+#define col_grn   "\x1b[32m"
+#define col_ylw  "\x1b[33m"
+#define col_blu    "\x1b[34m"
+#define col_mag "\x1b[35m"
+#define col_cyn    "\x1b[36m"
+#define col_reset   "\x1b[0m"
+
 
 /* expect this to be buggy, do mind it splits by spaces except quoted spaces
    escaped quotes do not function like a quote in the parser, but as a character */
@@ -68,32 +77,58 @@ char (*parse(char *pnt_cmd, int cmd_len, int *depth))[MAX_CHARS][MAX_ARGS] {
     return buffer;
 }
 
-char *getfile(char *filename, long int filesz) {
+int r_error(int type, char *error) { // error system
+    if (type == 1) {
+        printf(col_red"[err1]"col_reset " > %s", error);
+    } else {
+        printf("undetermined error\nerr type: %d\nerr message: %s");
+    }
+    printf("\n");
+    return 0;
+}
+
+int printls() {
+    char parent_dir[1024];
+    getcwd(parent_dir, sizeof(char [1024]));
+    return 0;
+}
+
+char *getfile(char *filename, long int *filesz) { // remember to free the pointer to buffer
     FILE *fptr = fopen(filename,"rb");
     fseek(fptr,0,SEEK_END);
-    filesz = ftell(fptr);
+    *filesz = ftell(fptr);
     rewind(fptr);
-    char *buffer = malloc(filesz + 1);
-    size_t read = fread(buffer, 1, filesz, fptr); // i honestly have no idea what this does i just found it on stackoverflow
+    char *buffer = malloc(*filesz + 1);
+    size_t read = fread(buffer, 1, *filesz, fptr); // i honestly have no idea what size_t does i just found it on stackoverflow
     buffer[read] = '\0'; // to my credit i understand this line to an extent
     return buffer;
 }
 
 int main() {
     printf("MIN7 PACKAGE MANAGER SHELL\nuse command `help` for a list of commands\n");
-
+    // init config
+    long int fsz;
+    char *fptr = getfile("resources/config.txt", &fsz);
+    char (*config)[MAX_CHARS][MAX_ARGS];
+    int configlen;
+    config = parse(fptr, fsz, &configlen);
+    free(fptr);
     while (true) { // main loop
+        // make useful stuff ahead of time
+        char wd[1024];
+        getcwd(wd, sizeof(char [1024]));
         // get commands
-        printf("%s", ">- ");
+        printf("%s>- ", wd);
         char cmd[64];
         fgets(cmd,64,stdin);
         cmd[strcspn(cmd, "\n")] = '\0';
         // printf("command: %s\n",cmd);
         int cmd_len = strlen(cmd);
+        int cmd_sz = sizeof(cmd);
         char (*splt_cmd)[MAX_CHARS][MAX_ARGS];
         int spltcmdl;
         splt_cmd = parse(cmd, cmd_len, &spltcmdl); // spltcmdl is the length of the splt_cmd array
-        /* printf("cmd args amt: %d\n", spltcmdl); // debugging purpose only
+        /* printf("cmd args amt: %d\n", spltcmdl); // debugging purpose only just ignore it i guess
         for (int i = 0;i < spltcmdl;i++) {
             printf("cmd part %d",i);
             printf(" = |%s|\n",(*splt_cmd)[i]);
@@ -105,8 +140,9 @@ int main() {
         } else 
         if (strcmp((*splt_cmd)[0], "help") == 0) {
             long int fsz;
-            char *fptr = getfile("resources/help.txt", *(&fsz)); // remember to free fptr, its a pointer to the file as a string
-            printf("%s\n\n",fptr);
+            char *fptr = getfile("resources/help.txt", &fsz); // remember to free fptr, its a pointer to the file as a string
+            printf("%s",fptr);
+            free(fptr);
         } else
         if (strcmp((*splt_cmd)[0], "cd") == 0) {
             if (chdir((*splt_cmd)[1]) != 0) {
@@ -117,27 +153,43 @@ int main() {
             }
         } else
         if (strcmp((*splt_cmd)[0], "pwd") == 0) {
-            char buffer[1024];
-            getcwd(buffer, sizeof(buffer));
-            printf("current working directory: %s\n", buffer);
+            printf("current working directory: %s\n", wd);
         } else
         if (strcmp((*splt_cmd)[0], "ls") == 0) {
-            if (strcmp((*splt_cmd)[1], "w") == 0) {
-                printf("using ls in powershell\n");
-                system("dir");
-            } else
-            if (strcmp((*splt_cmd)[1], "lx") == 0) {
-                printf("using ls in shell\n");
-                system("ls");
+            if (strcmp((*splt_cmd)[1], "os") == 0) {
+                if (strcmp((*config)[0], "w") == 0) {
+                    printf("using ls in powershell\n");
+                    system("dir");
+                } else
+                if (strcmp((*config)[0], "lx") == 0) {
+                    printf("using ls in shell\n");
+                    system("ls");
+                } else {
+                    r_error('1',"error in the config file, or invalid operating system in resources/config.txt");
+                }
+            
             } else {
-                printf("inbuilt list files function isnt finished yet sob\n");
+                if (printls() != 0) {
+
+                }
             }
         }
         // 404
         else {
-            printf("404 command not found\n");
+            char buffer[cmd_sz];
+            snprintf(buffer, cmd_sz, "command <%s> is not valid", cmd);
+            r_error(1,buffer);
         }
         free(splt_cmd);
+        free(config);
+        if ((*config)[1] == "y") {
+            long int fsz;
+            char *fptr = getfile("resources/config.txt", &fsz);
+            char (*config)[MAX_CHARS][MAX_ARGS];
+            int configlen;
+            config = parse(fptr, fsz, &configlen);
+            free(fptr);
+        }
     }
 
     return 0;
---
+++ src/resources/config_help.md
@@ -0,0 +1,8 @@
+# config docs, sort of??
+not really nescessary because there are commands for it, might be useful though.
+the config.txt is parsed as a command (read help.txt, dont use \n unless intentional), and isnt very readable.
+to my defense, it isnt supposed to be read at all
+
+## heres a neat list of what the args does:
+1. your operating system (w / lx)
+2. wether config should update between every command (y / n)
